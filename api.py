b3aa8b4c55b78cbe1d588875b4045d7f52bfb788
vinodP123 <vinodkumarPadidala@MacBook-Air-10.local>
2021-12-21 15:28:51 -0600

    Added api-python code for CRUD with requirements

---
+++ api.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify,request
+from random import randint,seed
+seed(5)
+
+app = Flask(__name__) #We need to define app for our server
+
+#Sample data
+Data = [{'ID':randint(1,140),
+         'name':'man',
+         'year':'1996'},
+        {'ID': randint(1,140),
+         'name': 'vin',
+         'year': '1992'},
+        ]
+
+@app.route('/',methods=['GET'])
+def home():
+    return 'Hello all'
+
+@app.route('/Students',methods=['GET'])
+def Students():
+    #return Data
+    return jsonify({'details':Data})
+
+@app.route('/Students/ind/<ID>',methods=['GET'])
+def ind(ID):
+    for s in Data:
+        if(str(s['ID'])==ID):
+            print(s)
+            student = s
+            #return s
+
+        #return 'Null'
+    #return jsonify({'details': student})
+    return student
+
+
+@app.route('/Students/upd/<ID>',methods=['PUT'])
+def upd(ID):
+    #'''
+    for s in Data:
+        #print('in')
+        #print(s)
+        if(str(s['ID'])==ID):
+            student = s
+            #print('in if')
+            #print(s)
+    #return jsonify({'details': s})
+    #return 'null'
+
+    #'''
+    if('ID' in request.json):
+        print('Student already exists')
+        #return 'Student already exists'
+    if('name' in request.json):
+        student['name'] = request.json['name']
+        return jsonify({"updated_student":student})
+    if('year' in request.json):
+        student['year'] = request.json['year']
+        return jsonify({"updated_student": student})
+
+@app.route('/Students/add',methods=['POST'])
+def add():
+    Student = {'ID':randint(1,140), #request.json['ID'],
+         'name': request.json['name'],
+         'year':request.json['year']}
+    Data.append(Student)
+    #return Data
+    #return jsonify({'details': Data}) #All the student details shown
+    return Student #Just created data is shown
+
+@app.route('/Students/rem/<ID>',methods=['DELETE'])
+def remove(ID):
+    for s in Data:
+        if str(s['ID'])==ID:
+            student = s
+            #po
+    if (student != 'Null'):
+        Data.remove(student)
+        #Data.pop(Student)
+    return 'Data removed for given ID'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
